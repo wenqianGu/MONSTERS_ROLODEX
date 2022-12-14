2556315abc6830d6fd38799cf0dd758de6340bc4
Lisa_GU <guwenqian10@gmail.com>
2022-12-15 09:45:06 +1100

    upload monster

--- src/App.js
+++ src/App.js
@@ -1,63 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import {Component} from 'react';
+import CardList from "./components/card-list/card-list.component";
 
+// constructor / renders on mount / renders whenever props change and setState gets called(componentDidMount).
 class App extends Component {
-  constructor() {
-    super();
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      monsters: [],
-      searchFiled: '',
+        this.state = {
+            monsters: [],
+            searchField: '',
+        }
+        //console.log('1-constructor');
     }
-    //console.log('1-constructor');
-  }
 
-  componentDidMount() {
-    //console.log('3-componentDidMount');
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState(
-        () => {
-          return { monsters: users }
-        },
-        () => { console.log(this.state) }))
-  }
-  onSearchChange = (event) => {
-    const searchFiled = event.target.value.toLocaleLowerCase();
+    componentDidMount() {
+        //console.log('3-componentDidMount');
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState(
+                () => {
+                    return {monsters: users}
+                }))
+    }
 
-    this.setState(() => {
-      return { searchFiled }
-    },
-      () => { console.log(searchFiled) })
-  }
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState(() => {
+            return {searchField};
+        });
+    };
 
-  render() {
-    //console.log('2-render');
-    //this is filter - filtering the filteredMonsters. 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchFiled)
-    });
+    render() {
+        console.log('render from AppJS')
+        const {monsters, searchField} =this.state;
+        const {onSearchChange} = this;
+        //this is filter - filtering the filteredMonsters.
+        const filteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchField);
+        });
 
-    return (
-      <div className='App'>
-        <input
-          className='search-box'
-          type='search'
-          placeholder='search monsters'
-          onChange={this.onSearchChange}
-        ></input>
-        {
-          filteredMonsters.map((monster) => {
-            return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            )
-          })
-        }</div >
-    )
-  }
+        return (
+            <div className='App'>
+                <input
+                    className='search-box'
+                    type='search'
+                    placeholder='search monsters'
+                    onChange={onSearchChange}
+                />
+                <CardList monsters={filteredMonsters}/>
+            </div>
+        )
+    }
 }
 
 export default App;
